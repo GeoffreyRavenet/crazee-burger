49cddb3df1fd79a28371771c31d7a825efb6bac2
GeoffreyRavenet <ravenet.geoffreypro@gmail.com>
2023-01-12 16:05:46 +0100

    ♻️ F01: LoginForm > replace handleClick with handleSubmit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,24 +1,29 @@
 import { useState } from "react";
 
 function LoginForm() {
-  //state {etat, données}
+  //state (etat, données)
   const [name, setName] = useState("")
+
   //comportements
-  const handleClick = () => {
-    return name? alert("Bonjour "+name) : null
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert(`Bonjour ${name}`)
+    setName("")
   }
+
   const handleChange = (event) => {
     setName(event.target.value)
   }
+  
   //affichage (render)
   return ( 
     <div>
-      <h1>Bienvenue chez nous!</h1>
+      <h1>Bienvenue chez nous !</h1>
       <br/>
       <h2>Connectez-vous</h2>
-      <form>
-        <input type="text" id="name" value={name} onChange={handleChange} placeholder="Entre votre prénom..." required/>
-        <button onClick={handleClick}>Accédez à votre espace</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={handleChange} placeholder="Entrer votre prénom..." required/>
+        <button>Accédez à votre espace</button>
       </form>
     </div>
   );
