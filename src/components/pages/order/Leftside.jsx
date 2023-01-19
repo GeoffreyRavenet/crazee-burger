32a1529f35811a5fb02cfae3098f93525d5ceb78
GeoffreyRavenet <ravenet.geoffreypro@gmail.com>
2023-01-19 14:51:56 +0100

    ✨ F04 : LeftSide > create Leftside feature

---
+++ src/components/pages/order/Leftside.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+import Logo from "../../reusable-ui/Logo.jsx"
+import { useNavigate } from "react-router"
+
+export default function Leftside() {
+  const navigate = useNavigate()
+
+  return (
+    <LeftSideStyled onClick={() => navigate(0)}>
+      <Logo />
+    </LeftSideStyled>
+  )
+}
+
+const LeftSideStyled = styled.div`
+  cursor: pointer;
+  margin-left: 20px;
+`
