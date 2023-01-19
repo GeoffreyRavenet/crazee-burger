bdbab42c792f82056178f6bc59464285d9635963
GeoffreyRavenet <ravenet.geoffreypro@gmail.com>
2023-01-19 15:51:01 +0100

    💄 F04 : Main > add box-shadow

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,3 +1,12 @@
+import styled from "styled-components"
+
 export default function Main() {
-  return <div>Main</div>
+  return <MainStyled>Main</MainStyled>
 }
+
+const MainStyled = styled.div`
+  min-height: 500px;
+  background: #f5f5f7;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  border-radius: 0px 0px 15px 15px;
+`
