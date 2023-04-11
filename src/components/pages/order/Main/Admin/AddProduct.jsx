7e3fad0e4ab0594fdd47c0c4b795708ae0084e19
GeoffreyRavenet <ravenet.geoffreypro@gmail.com>
2023-04-11 20:12:54 +0200

    ✨ F08 : AddProduct : create first form

--- src/components/pages/order/Main/Admin/AddProduct.jsx
+++ src/components/pages/order/Main/Admin/AddProduct.jsx
@@ -1,5 +1,54 @@
-import React from "react"
+import React, { useState } from "react"
+import { BsFillCameraFill } from "react-icons/bs"
+import { FaHamburger } from "react-icons/fa"
+import { MdOutlineEuro } from "react-icons/md"
+import TextInput from "../../../../reusable-ui/TextInput.jsx"
 
 export default function AddProduct() {
-  return <div>AddProduct</div>
+  const [product, setProduct] = useState({
+    id: 100000000,
+    imageSource: "/images/burger-vegan.png",
+    title: "Burger 1",
+    price: 5.297,
+    quantity: 0,
+    isAvailable: true,
+    isAdvertised: false,
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log("handleSubmit")
+  }
+
+  const handleChange = (event) => {
+    setProduct((product.title = event.target.value))
+  }
+
+  return (
+    //<img src={product.imageSource} alt="newproduct" />
+    <>
+      <form action="">
+        <TextInput
+          Icon={<FaHamburger className="icon" />}
+          value={product.title}
+          onChange={handleChange}
+          placeholder="Nom du produit (ex: Super Burger)"
+        />
+        <TextInput
+          Icon={<BsFillCameraFill className="icon" />}
+          value={product.imageSource}
+          onChange={handleChange}
+          placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
+        />
+        <TextInput
+          type="number"
+          Icon={<MdOutlineEuro className="icon" />}
+          value={product.price}
+          onChange={handleChange}
+          placeholder="Prix"
+        />
+        <button>Ajouter un nouveau produit au menu</button>
+      </form>
+    </>
+  )
 }
