f25e8418821bec2c713865f52f69ab5855a9a236
GeoffreyRavenet <ravenet.geoffreypro@gmail.com>
2023-04-19 13:38:33 +0200

    💄 F08 : EMPTY > style EmptyMenu

--- src/components/pages/order/Main/Menu/EmptyMenuAdmin.jsx
+++ src/components/pages/order/Main/Menu/EmptyMenuAdmin.jsx
@@ -1,5 +1,47 @@
-import React from "react"
+import styled from "styled-components"
+import PrimaryButton from "../../../../reusable-ui/PrimaryButton.jsx"
 
-export default function EmptyMenuAdmin() {
-  return <div>EmptyMenu</div>
+export default function EmptyMenuAdmin({ onReset }) {
+  return (
+    <EmptyMenuAdminStyled>
+      <div className="EmptyMenuTitle">Le menu est vide ?</div>
+      <div>Cliquez ci-dessous pour le réinitialiser</div>
+      <PrimaryButton
+        onClick={onReset}
+        label="Générer de nouveaux produits"
+        className="button-emptyAdmin"
+      />
+    </EmptyMenuAdminStyled>
+  )
 }
+
+const EmptyMenuAdminStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  font-family: "Amatic SC";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 45px;
+
+  color: #747b91;
+  div {
+    margin-bottom: 21px;
+  }
+  .EmptyMenuTitle {
+    font-weight: 700;
+  }
+  .button-emptyAdmin {
+    padding: 19px 25px 19px 25px;
+    height: 50px;
+
+    font-family: "Arial";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 12px;
+  }
+`
