bcecc4d18f9656b72803c454c38b951ef1061996
GeoffreyRavenet <ravenet.geoffreypro@gmail.com>
2023-04-24 15:24:11 +0200

    ✅ F11: BasketCard > create reuseble BasketCard

--- src/components/pages/order/Main/Basket/BasketMenu.jsx
+++ src/components/pages/order/Main/Basket/BasketMenu.jsx
@@ -1,7 +1,15 @@
 import React from "react"
 import styled from "styled-components"
+import BasketCard from "../../../../reusable-ui/BasketCard.jsx"
+import { formatPrice } from "../../../../../utils/maths.js"
 
 export default function BasketMenu() {
-  return <BasketMenuStyled>BasketMenu</BasketMenuStyled>
+  return (
+    <BasketMenuStyled>
+      <BasketCard title="test" price={formatPrice()} quantity="2" img="/images/wedges1.png" />
+    </BasketMenuStyled>
+  )
 }
-const BasketMenuStyled = styled.div``
+const BasketMenuStyled = styled.div`
+  margin: 20px 16px;
+`
---
+++ src/components/reusable-ui/BasketCard.jsx
@@ -0,0 +1,74 @@
+import React from "react"
+import styled from "styled-components"
+
+export default function BasketCard({ title, img, price, quantity }) {
+  return (
+    <BasketCardStyled>
+      <div className="image">
+        <img src={img} alt={title} />
+      </div>
+      <div className="infoProduct">
+        <div className="left-info">
+          <span className="title">{title}</span>
+          <span className="price">{price}</span>
+        </div>
+
+        <div className="right-info ">
+          <span className="quantity">x{quantity}</span>
+        </div>
+      </div>
+    </BasketCardStyled>
+  )
+}
+const BasketCardStyled = styled.div`
+  background: #ffffff;
+  box-shadow: -4px 4px 15px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  display: grid;
+  grid-template-columns: 30% 1fr;
+  padding: 0.5em 0.5em 0.5em 1em;
+  .image {
+    height: 60px;
+    img {
+      height: 100%;
+      width: 100%;
+      object-fit: contain;
+    }
+  }
+  .infoProduct {
+    margin-left: 14px;
+    display: grid;
+    grid-template-columns: 70% 1fr;
+    align-items: center;
+    .left-info {
+      display: grid;
+      white-space: nowrap;
+      .title {
+        font-family: "Amatic SC";
+        font-style: normal;
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 32px;
+
+        color: #17161a;
+      }
+      .price {
+        font-family: "Open Sans";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 15px;
+        line-height: 20px;
+        color: #ffa01b;
+      }
+    }
+    .quantity {
+      font-family: "Open Sans";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 22px;
+
+      color: #ffa01b;
+    }
+  }
+`
